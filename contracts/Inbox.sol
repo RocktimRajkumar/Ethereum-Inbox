85658d431b3614e1d40103c078fb28a5c497a0f9
Rocktim <rajkumar.rocktim@gmail.com>
2019-06-19 23:38:01 +0530

    contracts/Inbox.sol added,solc compile install,compile code written,gitignore added

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.19;
+
+contract Inbox{
+  string private message;
+  constructor(string initialMessage) public {
+      message = initialMessage;
+  }
+  function setMessage(string newMessage) public {
+      message = newMessage;
+  }
+  function getMessage() public view returns (string){
+      return message;
+  }
+}
